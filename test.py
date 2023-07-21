d8b587d9afcd9bb30b08bb1da11b084081c28ad4
ashrafshakerelbahalol <93045464+ashrafshakerelbahalol@users.noreply.github.com>
2023-07-21 18:54:24 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-from automatic import automtic_oranges
-
-sizeList, colorIndexList = automtic_oranges("4.bmp")
-m=0
-my_list = [[0 , 0, sizeList[i], 0, colorIndexList[i]] for i in range(len(sizeList))]
-print(my_list)
